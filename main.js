0477ecbe668be094344b3be9cb41c563937938ce
muhamejanov11 <muhamejanov.n@gmail.com>
2024-04-04 21:30:20 +0500

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+let num = +prompt('Напишите 6,10 или 15')
+if (num === 6) {
+    console.log('Делится на три');
+}
+
+else if (num === 10) {
+    console.log('Делится на пять');
+}
+
+else if (num === 15) {
+    console.log('Делится на три и на пять');
+}
